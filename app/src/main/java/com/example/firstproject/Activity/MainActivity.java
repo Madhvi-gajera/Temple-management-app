192ebe7e310e22691239bb7a8f45f4a0ac17c65d
dell <madhvigajera18@gmail.com>
2023-12-19 19:17:42 +0530

    Temple management app

---
+++ app/src/main/java/com/example/firstproject/Activity/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.firstproject.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.firstproject.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvMain = findViewById(R.id.tvMain);
+
+        tvMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
